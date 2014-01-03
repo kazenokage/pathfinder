ecc132648a0bef9f9dd0b4d7b6687a16d0edd327
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2014-01-03 15:40:34 +0900

    Additions to heap

--- src/Structures/MinHeap.java
+++ src/Structures/MinHeap.java
@@ -1,12 +1,31 @@
-
 package Structures;
 
-/**
- * 
- * Minimum heap.
- *
- * @author tomminikkanen
- */
 public class MinHeap {
 
+    private int[] heap;
+    private int size;
+    private int max;
+
+    public MinHeap(int max) {
+        this.max = max;
+        this.heap = new int[max];
+        size = 0;
+        heap[0] = 0;
+    }
+
+    private int left(int position) {
+        return 2 * position;
+    }
+
+    private int right(int position) {
+        return 2 * position + 1;
+    }
+    
+    private boolean leaf(int position) {
+        return ((position > size/2) && position <= size);
+    }
+    
+    private int parent(int position) {
+        return position/2;
+    }
 }
