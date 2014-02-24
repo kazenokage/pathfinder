04ba1b141eb1b761e3682e76a431caed30376089
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2014-02-24 21:44:47 +0200

    DInit done

---
+++ src/Structures/Dijkstra.java
@@ -0,0 +1,12 @@
+
+package Structures;
+
+
+public class Dijkstra implements Algorithm {
+
+    @Override
+    public Node[][] getNodes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ src/Tools/DInit.java
@@ -0,0 +1,23 @@
+package Tools;
+
+import Structures.DNode;
+
+public class DInit {
+
+    public DInit() {
+    }
+
+    public DNode[][] init(int[][] terrain) {
+        DNode[][] nodes = new DNode[terrain.length][terrain[0].length];
+
+        for (int i = 0; i < terrain.length; i++) {
+            for (int j = 0; j < terrain[0].length; j++) {
+                nodes[i][j] = new DNode(terrain[i][j], j, i);
+                nodes[i][j].setCostToThisPoint(Integer.MAX_VALUE / 2);
+            }
+        }
+        nodes[0][0].setCostToThisPoint(0);
+        
+        return nodes;
+    }
+}
